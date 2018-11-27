65e3ffdd8ce130be6859c56f5e78608977f2fb16
Svd115 <svd115@yahoo.fr>
2018-11-27 17:52:11 +0100

    'h'

--- api/models/forget/forget.js
+++ api/models/forget/forget.js
@@ -9,16 +9,13 @@
 		client.query('UPDATE users SET token=($1) WHERE email=($2) AND token IS NULL RETURNING id', [token, email], function(error, res){
 			client.end();
 			if(error){
-				console.log(error);
 				forget.emit("return", [false]);
 			}
 			else{
-				console.log(res.rows);
 				if(res.rows[0]){
 					forget.emit("return", [true, res.rows[0].id, token]);
 				}
 				else{
-					console.log("ici");
 					forget.emit("return", [false])
 				}
 			}
